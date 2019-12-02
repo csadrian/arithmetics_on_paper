483972b9da0eaffc3c13928d32768af75f73e378
csadrian <cs.adrian@gmail.com>
2019-12-02 19:00:54 +0100

    Seq eval test

--- eval_seq.py
+++ eval_seq.py
@@ -42,20 +42,31 @@ model.summary()
 def is_finished(state):
   return np.any(state == Symbols.end)
 
-for i, x in preprocess(dataset).take(args.eval_size).enumerate():
+np.set_printoptions(threshold=sys.maxsize)
 
-  state = x[0]
+for x in preprocess(dataset).take(args.eval_size):
+
+  state = x[0][1]
+  state = np.expand_dims(state, axis=0)
+  state_s = np.argmax(state, axis=-1)
+  print("state_s.shape:", state_s.shape)
   finished = False
   steps = 0
+
   while not finished and steps < 100:
-    res = model.predict(state)
-    res = np.argmax(res, axis=-1)
-    res = tf.one_hot(res, Symbols.NUM_SYMBOLS, axis=-1)
-    state += res
-    finished = is_finished(state)
+
+    pred = model.predict(state)
+    pred_s = np.argmax(pred, axis=-1)
+    finished = is_finished(pred_s)
+    state_s = state_s + pred_s
+    print("state_s.shape:", state_s.shape)
+
+    state = tf.one_hot(state_s, Symbols.NUM_SYMBOLS, axis=-1)
+    print(state_s)
+
+
     steps += 1
     print(steps)
-
-  print("Finished")
+  print("Finished in " + str(steps))
 
 
